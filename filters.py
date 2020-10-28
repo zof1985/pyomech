23aa559458cb44fa076f7e198216a3ef6f6f6578
Luca Zoffoli <zof1985@gmail.com>
2020-10-28 16:08:40 +0100

    update IMU pose estimation

--- filters.py
+++ filters.py
@@ -5,8 +5,10 @@
 
 import numpy as np
 import pandas as pd
-from . import vectors as pv
+import pyomech.vectors as pv
 import matplotlib.pyplot as pl
+import quaternion
+import scipy.spatial.transform as st
 
 
 
@@ -102,17 +104,19 @@ def rk4(t0, y0, dt, fun, **kwargs):
 class IMU():
 
 
-    def __init__(self, acc, gyr):
+    def __init__(self, acc, gyr, static_acc=None, static_gyr=None):
         """
         Inertial Measurement Unit class object.
 
         Input:
 
-            acc:    (pyomech.Vector)
-                    accelerometer data.
+            acc:        (pyomech.Vector)
+                        accelerometer data.
             
-            gyr:    (pyomech.Vector)
-                    gyroscope data.
+            gyr:        (pyomech.Vector)
+                        gyroscope data.
+            
+            static_acc: 
         """
         
         # check the entries
@@ -121,6 +125,8 @@ class IMU():
         self.accelerometer = acc
         assert isinstance(gyr, pv.Vector), txt.format("gyr")
         self.gyroscope = gyr / 180 * np.pi  # convert to rad/s
+        R = self.madgwickPose()
+        check = 1
     
 
     def madgwickPose(self, beta=0.06544985):
@@ -132,79 +138,67 @@ class IMU():
             beta:   (float)
                     the gain to be applied for compensating the gyro measurement error.
                     By default it is set at ~5°/sec, although it is provided in rad.
+        
+        Output:
+
+            R:      (dict)
+                    a dict with the time samples as keys containing a scipy.Rotation class
+                    object as value.
         """
 
+        # normalizer
+        def normalize(X):
+            """
+            normalize the array
+            """
+            return X / np.sqrt(np.sum(X ** 2))
+
+
         # initialize
-        A = self.accelerometer / self.accelerometer.module.values  # normalized accelerations
-        W = self.gyroscope.copy()  # it must be provided in rad/s
-        SEq = np.quaternion(1, 0, 0, 0)  # quaternion defining the orientation initiali conditions
+        SEq = np.quaternion(1, 0, 0, 0)  # quaternion defining the orientation initial conditions
+        R = {}
 
         # iterate the estimation
-        for n, i in enumerate(A.index.to_numpy()):
-            dt = (1 / A.sampling_frequency) if n == 0 else (i - A.index.to_numpy()[n - 1]) # get dt
+        for n, i in enumerate(self.accelerometer.index.to_numpy()):
+            
+            # get dt
+            if n == 0:
+                dt = 1 / self.accelerometer.sampling_frequency
+            else:
+                dt = i - self.accelerometer.index.to_numpy()[n - 1]
+
+            # get the accelerometer data
+            A = normalize(self.accelerometer.loc[i].values)
 
             # get the objective function
-            F = []
-    """
-    // Local system variables
-    float norm; // vector norm
-    float SEqDot_omega_1, SEqDot_omega_2, SEqDot_omega_3, SEqDot_omega_4; // quaternion derrivative from gyroscopes elements
-    float f_1, f_2, f_3; // objective function elements
-    float J_11or24, J_12or23, J_13or22, J_14or21, J_32, J_33; // objective function Jacobian elements
-    float SEqHatDot_1, SEqHatDot_2, SEqHatDot_3, SEqHatDot_4; // estimated direction of the gyroscope error
-    // Axulirary variables to avoid reapeated calcualtions
-    float halfSEq_1 = 0.5f * SEq_1;
-    float halfSEq_2 = 0.5f * SEq_2;
-    float halfSEq_3 = 0.5f * SEq_3;
-    float halfSEq_4 = 0.5f * SEq_4;
-    float twoSEq_1 = 2.0f * SEq_1;
-    float twoSEq_2 = 2.0f * SEq_2;
-    float twoSEq_3 = 2.0f * SEq_3;
-    // Normalise the accelerometer measurement
-    norm = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
-    a_x /= norm;
-    a_y /= norm;
-    a_z /= norm;
-    // Compute the objective function and Jacobian
-    f_1 = twoSEq_2 * SEq_4 - twoSEq_1 * SEq_3 - a_x;
-    f_2 = twoSEq_1 * SEq_2 + twoSEq_3 * SEq_4 - a_y;
-    f_3 = 1.0f - twoSEq_2 * SEq_2 - twoSEq_3 * SEq_3 - a_z;
-    J_11or24 = twoSEq_3; // J_11 negated in matrix multiplication
-    J_12or23 = 2.0f * SEq_4;
-    J_13or22 = twoSEq_1; // J_12 negated in matrix multiplication
-    J_14or21 = twoSEq_2;
-    J_32 = 2.0f * J_14or21; // negated in matrix multiplication
-    J_33 = 2.0f * J_11or24; // negated in matrix multiplication
-    // Compute the gradient (matrix multiplication)
-    SEqHatDot_1 = J_14or21 * f_2 - J_11or24 * f_1;
-    SEqHatDot_2 = J_12or23 * f_1 + J_13or22 * f_2 - J_32 * f_3;
-    SEqHatDot_3 = J_12or23 * f_2 - J_33 * f_3 - J_13or22 * f_1;
-    SEqHatDot_4 = J_14or21 * f_1 + J_11or24 * f_2;
-    // Normalise the gradient
-    norm = sqrt(SEqHatDot_1 * SEqHatDot_1 + SEqHatDot_2 * SEqHatDot_2 + SEqHatDot_3 * SEqHatDot_3 + SEqHatDot_4 * SEqHatDot_4);
-    SEqHatDot_1 /= norm;
-    SEqHatDot_2 /= norm;
-    SEqHatDot_3 /= norm;
-    SEqHatDot_4 /= norm;
-    // Compute the quaternion derrivative measured by gyroscopes
-    SEqDot_omega_1 = -halfSEq_2 * w_x - halfSEq_3 * w_y - halfSEq_4 * w_z;
-    SEqDot_omega_2 = halfSEq_1 * w_x + halfSEq_3 * w_z - halfSEq_4 * w_y;
-    SEqDot_omega_3 = halfSEq_1 * w_y - halfSEq_2 * w_z + halfSEq_4 * w_x;
-    SEqDot_omega_4 = halfSEq_1 * w_z + halfSEq_2 * w_y - halfSEq_3 * w_x;
-    // Compute then integrate the estimated quaternion derrivative
-    SEq_1 += (SEqDot_omega_1 - (beta * SEqHatDot_1)) * deltat;
-    SEq_2 += (SEqDot_omega_2 - (beta * SEqHatDot_2)) * deltat;
-    SEq_3 += (SEqDot_omega_3 - (beta * SEqHatDot_3)) * deltat;
-    SEq_4 += (SEqDot_omega_4 - (beta * SEqHatDot_4)) * deltat;
-    // Normalise quaternion
-    norm = sqrt(SEq_1 * SEq_1 + SEq_2 * SEq_2 + SEq_3 * SEq_3 + SEq_4 * SEq_4);
-    SEq_1 /= norm;
-    SEq_2 /= norm;
-    SEq_3 /= norm;
-    SEq_4 /= norm;
-    }
+            S = SEq.components
+            F = -A + 2 * np.array([[np.prod(S[[1, 3]]) - np.prod(S[[0, 2]])],
+                                   [np.prod(S[[0, 1]]) + np.prod(S[[2, 3]])],
+                                   [0.5 - np.sum(S[[1, 2]] ** 2)]])
+            
+            # get the Jacobian matrix
+            J = 2 * np.array([[-S[2],      S[3],     -S[0],  S[1]],
+                              [ S[1],      S[0],      S[3],  S[2]],
+                              [    0, -2 * S[1], -2 * S[2],     0]]).T
+            
+            # get the normalized gradient
+            SEqA = np.quaternion(*normalize(J.dot(F)).flatten())
+            
+            # get the estimate from the gyroscope
+            Wq = np.quaternion(0, *self.gyroscope.loc[i].values.flatten())
+            SEqW = 0.5 * SEq * Wq
 
-    """
+            # integrate and normalize
+            SEq_raw = SEq + (SEqW - (beta * SEqA)) * dt
+            SEq = SEq_raw / SEq_raw.norm()
+
+            # generate the Rotation class object
+            R[i] = st.Rotation.from_quat(SEq.components).to_euler('xyz')
+
+        # return the rotations
+        return R
+
+
 
     def integrateTime(self, V, S, dim_unit, type):
         """
