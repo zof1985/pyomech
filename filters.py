94685f470a72b216c50ebfe0a477ea9d0d5934ad
zof1985 <zof1985@gmail.com>
2020-10-28 07:35:29 +0100

    DO NOT USE

--- filters.py
+++ filters.py
@@ -135,24 +135,17 @@ class IMU():
         """
 
         # initialize
-        A = self.accelerometer.copy()
+        A = self.accelerometer / self.accelerometer.module.values  # normalized accelerations
         W = self.gyroscope.copy()  # it must be provided in rad/s
         SEq = np.quaternion(1, 0, 0, 0)  # quaternion defining the orientation initiali conditions
 
-        def filterUpdate()
-        """
-        // Math library required for ‘sqrt’
-#include <math.h>
-// System constants
-#define deltat 0.001f // sampling period in seconds (shown as 1 ms)
-#define gyroMeasError 3.14159265358979f * (5.0f / 180.0f) // gyroscope measurement error in rad/s (shown as 5 deg/s)
-#define beta sqrt(3.0f / 4.0f) * gyroMeasError // compute beta
-// Global system variables
-float a_x, a_y, a_z; // accelerometer measurements
-float w_x, w_y, w_z; // gyroscope measurements in rad/s
-float SEq_1 = 1.0f, SEq_2 = 0.0f, SEq_3 = 0.0f, SEq_4 = 0.0f; // estimated orientation quaternion elements with initial conditions
-void filterUpdate(float w_x, float w_y, float w_z, float a_x, float a_y, float a_z)
-{
+        # iterate the estimation
+        for n, i in enumerate(A.index.to_numpy()):
+            dt = (1 / A.sampling_frequency) if n == 0 else (i - A.index.to_numpy()[n - 1]) # get dt
+
+            # get the objective function
+            F = 
+"""
 // Local system variables
 float norm; // vector norm
 float SEqDot_omega_1, SEqDot_omega_2, SEqDot_omega_3, SEqDot_omega_4; // quaternion derrivative from gyroscopes elements
@@ -167,7 +160,7 @@ float halfSEq_4 = 0.5f * SEq_4;
 float twoSEq_1 = 2.0f * SEq_1;
 float twoSEq_2 = 2.0f * SEq_2;
 float twoSEq_3 = 2.0f * SEq_3;
-29
+"""
 // Normalise the accelerometer measurement
 norm = sqrt(a_x * a_x + a_y * a_y + a_z * a_z);
 a_x /= norm;
